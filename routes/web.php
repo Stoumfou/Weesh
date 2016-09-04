905d50c47dff9d8e49f508525e132137b3fb2dc7
Stoumfou <alexis.pambourg@gmail.com>
2016-09-04 19:19:21 +0200

    First Learning Commit
    Quizz 1.0

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,16 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'WelcomeController@index');
+
+Route::get('article/{n}', 'ArticleController@show')->where('n','[0-9]+');
+
+Route::get('facture/{n}', function($n) {
+    return view('facture')->withNumero($n);
+})->where('n', '[0-9]+');
+
+Route::get('users', 'UsersController@getInfos');
+Route::post('users', 'UsersController@postInfos');
+
+Route::get('contact', 'ContactController@getForm');
+Route::post('contact', 'ContactController@postForm');
