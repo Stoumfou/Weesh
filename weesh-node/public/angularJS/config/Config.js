9ed40fe08c9ab33edfe5355d7a8bf573a418f621
Clément LEPERE <lepere.clement@gmail.com>
2017-01-20 17:43:22 +0100

    Ajout des routes et de la page de connexion fonctionnelle
    MAKE YOUR WEEK END COME TRUE <3

--- weesh-node/public/angularJS/config/Config.js
+++ weesh-node/public/angularJS/config/Config.js
@@ -1,7 +1,9 @@
 angular.module('weesh').config([
         '$stateProvider'
         , '$urlRouterProvider'
-        
+
+
+    
     , function ($stateProvider, $urlRouterProvider) {
         $stateProvider.state('home', {
             url: '/home'
@@ -10,6 +12,33 @@ angular.module('weesh').config([
         }).state('login', {
             url: '/login'
             , templateUrl: '/login.ejs'
+            , controller: 'AuthCtrl'
+            , onEnter: ['$state', 'auth', function ($state, auth) {
+                if (auth.isLoggedIn()) {
+                    $state.go('home');
+                }
+                    }]
+        }).state('myLists', {
+            url: '/myLists'
+            , templateUrl: '/myLists.ejs'
+            , controller: 'AuthCtrl'
+            , onEnter: ['$state', 'auth', function ($state, auth) {
+                if (auth.isLoggedIn()) {
+                    $state.go('home');
+                }
+                    }]
+        }).state('mySettings', {
+            url: '/mySettings'
+            , templateUrl: '/mySettings.ejs'
+            , controller: 'AuthCtrl'
+            , onEnter: ['$state', 'auth', function ($state, auth) {
+                if (auth.isLoggedIn()) {
+                    $state.go('home');
+                }
+                    }]
+        }).state('myProducts', {
+            url: '/myProducts'
+            , templateUrl: '/myProducts.ejs'
             , controller: 'AuthCtrl'
             , onEnter: ['$state', 'auth', function ($state, auth) {
                 if (auth.isLoggedIn()) {
