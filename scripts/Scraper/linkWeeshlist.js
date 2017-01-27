870dcd1290f051694df6188692331fe8beb191eb
Unknown <sami.atallah@live.fr>
2017-01-27 03:50:30 +0100

    Correction précédent

--- scripts/Scraper/linkWeeshlist.js
+++ scripts/Scraper/linkWeeshlist.js
@@ -16,23 +16,25 @@ var mysql = _mysql.createConnection({
         
 function linkItemToWeeshlist(){
 	var weeshlist = "INSERT INTO weeshlists(id, user_id, name, visibility) VALUES (1, 1, 'Weeshlist de Sami', 'private');";
-	_mysql.query(weeshlist, function (error, results, fields) {
+	mysql.query(weeshlist, function (error, results, fields) {
     	if(error) throw error;
     	console.log('\x1b[32m%s\x1b[0m', results); // Ecrit en vert GJ
 	});
 
 	var lnk = "INSERT INTO itemslnkweeshlists(item, weeshlist) VALUES (40, 1);";
-	_mysql.query(lnk, function (error, results, fields) {
+	mysql.query(lnk, function (error, results, fields) {
     	if(error) throw error;
     	console.log('\x1b[32m%s\x1b[0m', results); // Ecrit en vert GJ
 	});
 
 	var select = "SELECT * from itemslnkweeshlists";
-	_mysql.query(select, function (error, results, fields) {
+	mysql.query(select, function (error, results, fields) {
     	if(error) throw error;
     	console.log('\x1b[32m%s\x1b[0m', results); // Ecrit en vert GJ
 	});
 }
+
+linkItemToWeeshlist();
 
 
 mysql.end();
